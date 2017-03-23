90e80dedfe80cbf0b37de9ac71af81ef106a70f6
Zyuzin Vitaly <juwain@yandex.ru>
2017-03-23 14:46:03 +0300

    Правит кнопки на странице одной гифки

--- resources/views/gif/view.php
+++ resources/views/gif/view.php
@@ -31,8 +31,8 @@ $comments = $commentModel->findAllByField('user_id', $gif['user_id']);
 
         <?php if ($user): ?>
         <div class="gif__controls">
-            <button class="button gif__control" type="button">Мне нравится</button>
-            <button class="button gif__control" type="button">В избранное</button>
+            <a class="button gif__control" href="/">Мне нравится</a>
+            <a class="button gif__control" href="/">В избранное</a>
         </div>
         <?php endif; ?>
     </div>
