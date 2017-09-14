6c490b1c5b944a257a61085fca25af731e7b2dd2
Kirill <sks89@mail.ru>
2017-09-14 21:53:14 +0300

    правит демки

--- examples/state_3/init.php
+++ examples/state_3/init.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'functions.php';
 
-$link = mysqli_connect("localhost", "giftube_edu", "cerMLxxY", "gifube_edu");
+//$link = mysqli_connect("localhost", "giftube_edu", "cerMLxxY", "gifube_edu");
+$link = mysqli_connect("localhost", "root", "", "giftube");
 
 $categories = [];
 $content = '';
--- templates/index.php
+++ templates/index.php
@@ -22,7 +22,7 @@
 
         <form class="search" action="/search.php" method="get">
             <div class="search__control">
-                <input class="search__text" type="text" name="q" value="" placeholder="Поиск гифки…">
+                <input class="search__text" type="text" name="q" value="<?=$_GET['q'] ?? '';?>" placeholder="Поиск гифки…">
 
                 <div class="search__submit">
                     <input class="button" type="submit" name="" value="Найти">
