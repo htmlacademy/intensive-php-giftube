897e50582b92483234a021b3633fb639385c6e78
Kirill <sks89@mail.ru>
2017-11-06 19:21:14 +0300

    Удаляет старое API для демок Меняет конфиг для подключения к БД в примерах

--- examples/state_3/init.php
+++ examples/state_3/init.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'functions.php';
+$db = require_once 'config/db.php';
 
-$link = mysqli_connect("localhost", "giftube_edu", "cerMLxxY", "gifube_edu");
+$link = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
 
 $categories = [];
 $content = '';
--- examples/state_5/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'functions.php';
-
-$link = mysqli_connect("localhost", "giftube_edu", "cerMLxxY", "gifube_edu");
-
-$categories = [];
-$content = '';
