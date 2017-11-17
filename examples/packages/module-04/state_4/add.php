10becf687260bc7b761cc487fb5e483f20070569
Kirill <sks89@mail.ru>
2017-11-17 17:34:16 +0300

    Добавляет разметку шагов последней демки

--- examples/packages/module-04/state_4/add.php
+++ examples/packages/module-04/state_4/add.php
@@ -1,13 +1,16 @@
 <?php
 require_once('functions.php');
 
+/* BEGIN STATE 01 */
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	/* BEGIN STATE 02 */
 	$gif = $_POST;
 
 	$required = ['title', 'description'];
 	$dict = ['title' => 'Название', 'description' => 'Описание'];
 	$errors = [];
-
+/* END STATE 02 */
+/* BEGIN STATE 03 */
 	foreach ($_POST as $key => $value) {
 		if (in_array($key, $required)) {
 			if (!$value) {
@@ -15,38 +18,55 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			}
 		}
 	}
+	/* END STATE 03 */
 
+	/* BEGIN STATE 04 */
 	if (isset($_FILES['gif_img']['name'])) {
 		$tmp_name = $_FILES['gif_img']['tmp_name'];
 		$path = $_FILES['gif_img']['name'];
 
+		/* BEGIN STATE 05 */
 		$finfo = finfo_open(FILEINFO_MIME_TYPE);
 		$file_type = finfo_file($finfo, $tmp_name);
-
+		/* END STATE 05 */
+		/* BEGIN STATE 06 */
 		if ($file_type !== "image/gif") {
 			$errors['Файл'] = 'Загрузите картинку в формате GIF';
 		}
+		/* END STATE 06 */
+		/* BEGIN STATE 07 */
 		else {
 			move_uploaded_file($tmp_name, 'uploads/' . $path);
 			$gif['path'] = $path;
-
 		}
+		/* END STATE 07 */
 	}
+	/* END STATE 04 */
+	/* BEGIN STATE 08 */
 	else {
 		$errors['Файл'] = 'Вы не загрузили файл';
 	}
+	/* END STATE 08 */
 
+	/* BEGIN STATE 09 */
 	if (count($errors)) {
 		$page_content = include_template('add.php', ['gif' => $gif, 'errors' => $errors]);
 	}
+	/* END STATE 09 */
+	/* BEGIN STATE 10 */
 	else {
 		$page_content = include_template('view.php', ['gif' => $gif]);
 	}
+	/* END STATE 10 */
 }
+/* END STATE 01 */
+/* BEGIN STATE 11 */
 else {
 	$page_content = include_template('add.php', []);
 }
+/* END STATE 11 */
 
+/* BEGIN STATE 12 */
 $layout_content = include_template('layout.php', [
 	'content'    => $page_content,
 	'categories' => [],
@@ -54,3 +74,4 @@ $layout_content = include_template('layout.php', [
 ]);
 
 print($layout_content);
+/* END STATE 12 */
