7bcc7282e7a6dbf90ca3d65464996e7530047073
Kirill Senkevich <frexin@htmlacademy.ru>
2018-02-06 22:01:27 +0700

    Добавляет демки для раздела про работу с датой и временем

---
+++ examples/packages/module-03/state_5/date.php
@@ -0,0 +1,37 @@
+<?php
+date_default_timezone_set("Europe/Moscow");
+
+/* END STATE 01 */
+// Покажем текущую дату
+$curdate = date('d.m.Y');
+print("Текущая дата: $curdate<br>");
+/* END STATE 01 */
+
+/* END STATE 02 */
+// Покажем текущее время
+$curtime = date('H:i:s');
+print("Текущее время: $curtime<br>");
+/* END STATE 02 */
+
+/* BEGIN STATE 03 */
+// Сколько времени сейчас в NY
+date_default_timezone_set("America/New_York");
+$nytime = $curtime = date('H:i:s');
+print("Время в Нью-Йорке: $nytime<br>");
+/* END STATE 03 */
+
+/* BEGIN STATE 04 */
+setlocale(LC_ALL, 'ru_RU');
+// Какой сейчас день недели
+$dow = strftime("%A");
+print("День недели: $dow<br>");
+/* END STATE 04 */
+
+/* BEGIN STATE 05 */
+// Сколько осталось дней до нового года
+$curday = date('z');
+$ydc = date('L') ? 366 : 365;
+$days_remaining = $ydc - $curday;
+
+print("До нового года осталось дней: $days_remaining");
+/* END STATE 05 */
---
+++ examples/packages/module-03/state_5/timestamp.php
@@ -0,0 +1,38 @@
+<?php
+date_default_timezone_set("Europe/Moscow");
+
+/* BEGIN STATE 01 */
+// Получить текущий timestamp
+$ts = time();
+
+print("С начала 1970 года прошло $ts секунд!<br>");
+/* END STATE 01 */
+
+/* BEGIN STATE 02 */
+// Узнаем когда закончится интенсив
+$int_end = "7 March 2018";
+$end_ts = strtotime($int_end);
+
+print("TS для окончания интенсива: $end_ts<br>");
+/* END STATE 02 */
+
+/* BEGIN STATE 03 */
+// В одном дне 86400 секунд
+$secs_in_day = 86400;
+
+$ts_diff = $end_ts - $ts;
+$days_until_end = floor($ts_diff / $secs_in_day);
+
+print("До окончания интенсива осталось дней: $days_until_end<br>");
+/* END STATE 03 */
+
+/* BEGIN STATE 04 */
+// TS для полночи
+$ts_midnight = strtotime('tomorrow');
+$secs_to_midnight = $ts_midnight - $ts;
+
+$hours = floor($secs_to_midnight / 3600);
+$minutes = floor(($secs_to_midnight % 3600) / 60);
+
+print("До полночи осталось: $hours часов и $minutes минут");
+/* END STATE 04 */
