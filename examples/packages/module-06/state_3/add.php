9a92087156032520858b1f4c0a5cf23065f1fdd1
Kirill Senkevich <frexin@htmlacademy.ru>
2018-04-13 16:32:25 +0700

    Адаптирует демонстрации к новой программе

--- examples/packages/module-06/state_3/add.php
+++ examples/packages/module-06/state_3/add.php
@@ -1,42 +1,53 @@
 <?php
-require_once('functions.php');
+require_once 'init.php';
 
-/* BEGIN STATE 01 */
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$gif = $_POST;
-
-	/* BEGIN STATE 02 */
-	if (isset($_FILES['gif_img']['name'])) {
-		$path = $_FILES['gif_img']['name'];
-		/* BEGIN STATE 03 */
-		$res = move_uploaded_file($_FILES['gif_img']['tmp_name'], 'uploads/' . $path);
-		/* END STATE 03 */
-	}
-	/* END STATE 02 */
-
-	/* BEGIN STATE 04 */
-	if (isset($path)) {
-		$gif['path'] = $path;
-	}
-	/* END STATE 04 */
-
-	/* BEGIN STATE 05 */
-	$page_content = include_template('view.php', ['gif' => $gif]);
-	/* END STATE 05 */
+if (!$link) {
+    $error = mysqli_connect_error();
+    show_error($content, $error);
 }
-/* END STATE 01 */
-/* BEGIN STATE 06 */
 else {
-	$page_content = include_template('add.php', []);
+    $sql = 'SELECT `id`, `name` FROM categories';
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+    else {
+        $error = mysqli_error($link);
+        show_error($content, $error);
+    }
+
+    $content = include_template('add.php', ['categories' => $categories]);
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $gif = $_POST['gif'];
+
+        $filename = uniqid() . '.gif';
+        $gif['path'] = $filename;
+        move_uploaded_file($_FILES['gif_img']['tmp_name'], 'uploads/' . $filename);
+
+        /* BEGIN STATE 01 */
+        $sql = 'INSERT INTO gifs (dt_add, category_id, user_id, title, description, path) VALUES (NOW(), ?, 1, ?, ?, ?)';
+        /* END STATE 01 */
+
+        /* BEGIN STATE 02 */
+        $stmt = db_get_prepare_stmt($link, $sql, [$gif['category'], $gif['title'], $gif['description'], $gif['path']]);
+        $res = mysqli_stmt_execute($stmt);
+        /* END STATE 02 */
+
+        /* BEGIN STATE 03 */
+        if ($res) {
+            $gif_id = mysqli_insert_id($link);
+
+            header("Location: gif.php?id=" . $gif_id);
+        }
+        /* END STATE 03 */
+        /* BEGIN STATE 04 */
+        else {
+            $content = include_template('error.php', ['error' => mysqli_error($link)]);
+        }
+        /* END STATE 04 */
+    }
 }
-/* END STATE 06 */
 
-/* BEGIN STATE 07 */
-$layout_content = include_template('layout.php', [
-	'content'    => $page_content,
-	'categories' => [],
-	'title'      => 'GifTube - Добавление гифки'
-]);
-
-print($layout_content);
-/* END STATE 07 */
+print include_template('index.php', ['content' => $content, 'categories' => $categories]);
