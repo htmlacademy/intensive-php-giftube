b6de0269fb1666d17227177e66f464f4634c11ea
Senkevich Kirill <sks89@mail.ru>
2018-12-07 16:56:25 +0300

    Дорабатывает демонстрацию 7/3

--- examples/packages/module-07/state_3/register.php
+++ examples/packages/module-07/state_3/register.php
@@ -8,34 +8,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $form = $_POST['signup'];
     $errors = [];
 
-    /* BEGIN STATE 01 */
-    $email = mysqli_real_escape_string($link, $form['email']);
-    $sql = "SELECT id FROM users WHERE email = '$email'";
-    $res = mysqli_query($link, $sql);
-    /* END STATE 01 */
+    $req_fields = ['email', 'password', 'name'];
 
-
-    /* BEGIN STATE 02 */
-    if (mysqli_num_rows($res) > 0) {
-        $errors[] = 'Пользователь с этим email уже зарегистрирован';
+    foreach ($req_fields as $field) {
+        if (empty($form[$field])) {
+            $errors[] = "Не заполнено поле " . $field;
+        }
     }
-    /* END STATE 02 */
-    /* BEGIN STATE 03 */
-    else {
-        $password = password_hash($form['password'], PASSWORD_DEFAULT);
 
-        $sql = 'INSERT INTO users (dt_add, email, name, password, token) VALUES (NOW(), ?, ?, ?, "")';
-        $stmt = db_get_prepare_stmt($link, $sql, [$form['email'], $form['name'], $password]);
-        $res = mysqli_stmt_execute($stmt);
-    }
-    /* END STATE 03 */
+    if (empty($errors)) {
+        /* BEGIN STATE 01 */
+        $email = mysqli_real_escape_string($link, $form['email']);
+        $sql = "SELECT id FROM users WHERE email = '$email'";
+        $res = mysqli_query($link, $sql);
+        /* END STATE 01 */
 
-    /* BEGIN STATE 04 */
-    if ($res && empty($errors)) {
-        header("Location: /enter.php");
-        exit();
+        /* BEGIN STATE 02 */
+        if (mysqli_num_rows($res) > 0) {
+            $errors[] = 'Пользователь с этим email уже зарегистрирован';
+        }
+        /* END STATE 02 */
+        /* BEGIN STATE 03 */
+        else {
+            $password = password_hash($form['password'], PASSWORD_DEFAULT);
+
+            $sql = 'INSERT INTO users (dt_add, email, name, password, token) VALUES (NOW(), ?, ?, ?, "")';
+            $stmt = db_get_prepare_stmt($link, $sql, [$form['email'], $form['name'], $password]);
+            $res = mysqli_stmt_execute($stmt);
+        }
+        /* END STATE 03 */
+
+        /* BEGIN STATE 04 */
+        if ($res && empty($errors)) {
+            header("Location: /enter.php");
+            exit();
+        }
+        /* END STATE 04 */
     }
-    /* END STATE 04 */
 
     /* BEGIN STATE 05 */
     $tpl_data['errors'] = $errors;
