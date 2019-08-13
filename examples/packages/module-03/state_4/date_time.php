35bc5f4aa277453c00bbfcecafd0c7b03d21f4fa
Senkevich Kirill <sks89@mail.ru>
2019-08-13 13:53:56 +0300

    Правки по демонстрациям 3-го раздела

--- examples/packages/module-03/state_4/date_time.php
+++ examples/packages/module-03/state_4/date_time.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Europe/Moscow");
 setlocale(LC_ALL, 'ru_RU');
 
 /* BEGIN STATE 01 */
-$int_end_str = "10.06.2019";
+$int_end_str = "06.10.2019";
 /* END STATE 01 */
 /* BEGIN STATE 02 */
 $dt_end = date_create($int_end_str);
