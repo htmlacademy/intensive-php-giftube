33928dec8733b1bba3bcfc6d7b087361f145cf4b
Senkevich Kirill <frexin@gmail.com>
2022-04-20 21:32:20 +0700

    Заменяет swiftmailer на symfony mailer

--- live/05-final/mail.php
+++ live/05-final/mail.php
@@ -1,47 +1,27 @@
 <?php
-require_once 'vendor/autoload.php';
-require_once '../common/init.php';
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
 
+require 'vendor/autoload.php';
 
-$transport = new Swift_SmtpTransport("smtp.mailtrap.io", 2525);
-$transport->setUsername("75f3c8c888f4c0");
-$transport->setPassword("d3bf00f9a2376d");
+//<editor-fold desc="dsn">
+// Конфигурация траспорта
+$dsn = 'smtp://75f3c8c888f4c0:d3bf00f9a2376d@smtp.mailtrap.io:2525?encryption=tls&auth_mode=login';
+$transport = Transport::fromDsn($dsn);
+//</editor-fold>
 
-$mailer = new Swift_Mailer($transport);
+//<editor-fold desc="message">
+// Формирование сообщения
+$message = new Email();
+$message->to("sks89@mail.ru");
+$message->from("mail@giftube.academy");
+$message->subject("Просмотры вашей гифки");
+$message->text("Вашу гифку «Кот и пылесос» посмотрело больше 1 млн!");
+//</editor-fold>
 
-$sql = "SELECT id, title, show_count, path FROM gifs g WHERE MONTH(dt_add) = MONTH(NOW()) "
-     . "AND YEAR(dt_add) = YEAR(NOW()) ORDER BY show_count DESC LIMIT 3";
-
-$res = mysqli_query($link, $sql);
-
-if ($res && mysqli_num_rows($res)) {
-    $gifs = mysqli_fetch_all($res, MYSQLI_ASSOC);
-
-    $res = mysqli_query($link, "SELECT email, name FROM users");
-
-    if ($res && mysqli_num_rows($res)) {
-        $users = mysqli_fetch_all($res, MYSQLI_ASSOC);
-        $recipients = [];
-
-        foreach ($users as $user) {
-            $recipients[$user['email']] = $user['name'];
-        }
-
-        $message = new Swift_Message();
-        $message->setSubject("Самые горячие гифки за этот месяц");
-        $message->setFrom(['keks@phpdemo.ru' => 'GifTube']);
-        $message->setTo($recipients);
-
-        $msg_content = include_template('month_email.php', ['gifs' => $gifs]);
-        $message->setBody($msg_content, 'text/html');
-
-        $result = $mailer->send($message);
-
-        if ($result) {
-            print("Рассылка успешно отправлена");
-        }
-        else {
-            print("Не удалось отправить рассылку");
-        }
-    }
-}
+//<editor-fold desc="send">
+// Отправка сообщения
+$mailer = new Mailer($transport);
+$mailer->send($message);
+//</editor-fold>
